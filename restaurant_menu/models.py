cc8b3d75995d1ebc9cab25f17d83af57d636bca3
mahmudurmahid <m.mahid@icloud.com>
2025-05-06 10:55:21 +0100

    added max_length to meal_type in food item model

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -18,7 +18,7 @@ class FoodItem(models.Model):
     meal = models.CharField(max_length=1000, unique=True)
     description = models.CharField(max_length=2000)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    meal_type = models.CharField(choices=MEAL_TYPE)
+    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     food_availability = models.IntegerField(choices=FOOD_AVAILABILITY, default=0)
     date_created = models.DateTimeField(auto_now_add=True)
