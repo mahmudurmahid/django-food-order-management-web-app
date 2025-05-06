2134e61378d38a33c66d882aa12a96eb819c74c2
mahmudurmahid <m.mahid@icloud.com>
2025-05-06 11:12:15 +0100

    views for menu list and menu items

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import FoodItem
 
 # Create your views here.
+class MenuListView(generic.ListView):
+    queryset = FoodItem.object.order_by("-date_created")
+    template_name = "index.html"
+
+
+class MenuItemDetail(generic.DetailView):
+    model = FoodItem
+    template_name = "menu_item_detail.html"
+
+
