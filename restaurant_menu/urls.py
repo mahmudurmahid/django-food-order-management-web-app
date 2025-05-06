dae4794375e3a75f51ef7388fa13bd8690d7b53c
mahmudurmahid <m.mahid@icloud.com>
2025-05-06 11:50:56 +0100

    testing context from views into html pages

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MenuListView.as_view(), name="home"),
+    path("", views.MenuList.as_view(), name="home"),
     path("", views.MenuItemDetail.as_view(), name="menu_item_detail"),
 ]
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -3,13 +3,16 @@ from django.views import generic
 from .models import FoodItem
 
 # Create your views here.
-class MenuListView(generic.ListView):
+class MenuList(generic.ListView):
     queryset = FoodItem.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "Pasta", "Lasagna", "Salad"],
+                   "ingredients": ["Tomato", "Cheese", "Basil", "Olive Oil"]
+                   }
+        return context
 
 class MenuItemDetail(generic.DetailView):
     model = FoodItem
     template_name = "menu_item_detail.html"
-
-
