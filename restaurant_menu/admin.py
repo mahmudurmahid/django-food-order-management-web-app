ed094f0ad8c48b9026b0073574feb7ccee84bbac
mahmudurmahid <m.mahid@icloud.com>
2025-05-06 12:34:47 +0100

    admin interface created

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import FoodItem
 
 # Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "food_availability", "meal_type", "price", "author")
+    list_filter = ("food_availability",)
+    search_fields = ("meal", "description")
+
+admin.site.register(FoodItem, MenuItemAdmin)
