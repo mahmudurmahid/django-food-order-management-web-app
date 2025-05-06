2a2dca98806072faaf1c28c10ba48514a0b4d148
mahmudurmahid <m.mahid@icloud.com>
2025-05-06 13:04:40 +0100

    food items shown on homepage

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import FoodItem
+from .models import FoodItem, MEAL_TYPE
 
 # Create your views here.
 class MenuList(generic.ListView):
     queryset = FoodItem.objects.order_by("-date_created")
     template_name = "index.html"
 
-    def get_context_data(self):
-        context = {"meals": ["Pizza", "Pasta", "Lasagna", "Salad"],
-                   "ingredients": ["Tomato", "Cheese", "Basil", "Olive Oil"]
-                   }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
         return context
 
 class MenuItemDetail(generic.DetailView):
